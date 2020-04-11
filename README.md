e806be0e33e85549f692a6ef24615acfa85bdaaf
HimbeersaftLP <mail@himbeer.me>
2020-04-11 15:36:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# HideCommands
+[![Poggit-Ci](https://poggit.pmmp.io/ci.shield/HimbeersaftLP/HideCommands/HideCommands)](https://poggit.pmmp.io/ci/HimbeersaftLP/HideCommands/HideCommands)
+[![](https://poggit.pmmp.io/shield.state/HideCommands)](https://poggit.pmmp.io/p/HideCommands)
+
+Remove specific commands from the in-game command suggestions.
+
+Note: Just because the commands are hidden, doesn't mean they can't be executed. You still need to make sure to have proper permission management in place.
+
+Icon credits: https://pixabay.com/vectors/spy-hat-anony-anonymous-detect-2657484/
---
+++ src/Himbeer/HideCommands/Main.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Himbeer\HideCommands;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	private const MODE_WHITELIST = 0;
+	private const MODE_BLACKLIST = 1;
+
+	/** @var int */
+	private $mode;
+	/** @var array  */
+	private $commandList = [];
+
+	public function onEnable() {
+		$this->saveDefaultConfig();
+
+		switch($this->getConfig()->get("mode")) {
+			case "whitelist":
+				$this->mode	= self::MODE_WHITELIST;
+				break;
+			case "blacklist":
+				$this->mode	= self::MODE_BLACKLIST;
+				break;
+			default:
+				$this->getLogger()->error('Invalid mode selected, must be either "blacklist" or "whitelist"! Disabling...');
+				$this->setEnabled(false);
+				return;
+		}
+
+		foreach ($this->getConfig()->get("commands") as $command) {
+			// We put the command name in the key so we can use array_intersect_key and array_diff_key
+			$this->commandList[strtolower($command)] = null;
+		}
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onDataPacketSend(DataPacketSendEvent $event) {
+		$packet = $event->getPacket();
+		if ($packet instanceof AvailableCommandsPacket) {
+			if ($this->mode === self::MODE_WHITELIST) {
+				$packet->commandData = array_intersect_key($packet->commandData, $this->commandList);
+			} else {
+				$packet->commandData = array_diff_key($packet->commandData, $this->commandList);
+			}
+		}
+	}
+}
